3a690c2d6ea2bd08192a9cdda36dcf147918aa5e
Rohit Verma <rock.verma.rohit@gmail.com>
2021-01-19 14:19:17 +0530

    Update handleflow.js

--- src/FB/controller/handleflow.js
+++ src/FB/controller/handleflow.js
@@ -1,86 +1,66 @@
 require("dotenv").config();
+const comp = require('../component/comp');
 const sendMessage = require("../component/send");
 
 let handleMessage = (sender_psid, received_message) => {
+    let response
 
     if (received_message.text) {
 
         let textmessage = received_message.text;
-        handleText(sender_psid, textmessage)
+        switch (textmessage) {
+            case "hi":
+                response = { "text": 'hello' }
+                break;
+            case "bye":
+                response = { "text": 'bye bye' }
+                break;
+            default:
+                response = { "text": 'you sent me ' + textmessage }
+
+        }
 
     }
 
     else if (received_message.attachments) {
 
         let attachment_url = received_message.attachments[0].payload.url;
-        handleAttachments(sender_psid, attachment_url)
 
-
+        response = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Is this the right?",
+                        "subtitle": "Tap a button to answer.",
+                        "image_url": attachment_url,
+                        "buttons": [
+                            {
+                                "type": "postback",
+                                "title": "Yes!",
+                                "payload": "yes",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "No!",
+                                "payload": "no",
+                            }
+                        ],
+                    }]
+                }
+            }
+        }
     }
 
     else if (received_message && received_message.quick_reply && received_message.quick_reply.payload) {
 
         let payload = received_message.quick_reply.payload;
-        handleQuickreply(sender_psid, payload)
-
-    }
-
-}
-
-function handleText(sender_psid, textmessage) {
-    let response
-
-    response = {
-        "text": 'you sent me ' + textmessage
-    }
-
-    sendMessage(sender_psid, response)
-
-}
-
-function handleAttachments(sender_psid, attachment_url) {
-    let response
-
-    response = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Is this the right?",
-                    "subtitle": "Tap a button to answer.",
-                    "image_url": attachment_url,
-                    "buttons": [
-                        {
-                            "type": "postback",
-                            "title": "Yes!",
-                            "payload": "yes",
-                        },
-                        {
-                            "type": "postback",
-                            "title": "No!",
-                            "payload": "no",
-                        }
-                    ],
-                }]
-            }
+        response = {
+            "text": 'you sent me ' + payload
         }
     }
-
     sendMessage(sender_psid, response)
-
-}
-
-function handleQuickreply(sender_psid, payload) {
-    let response
-
-    response = {
-        "text": 'you sent me Quickreply'
-    }
-
-    sendMessage(sender_psid, response)
-
-
 }
 
 let handlePostback = (sender_psid, received_postback) => {
@@ -90,17 +70,16 @@ let handlePostback = (sender_psid, received_postback) => {
     // Set the response based on the postback payload
     switch (payload) {
         case "yes":
-            response = {
-                "text": 'you pressed YES'
-            }
+            response = { "text": 'you pressed YES' }
             break;
         case "no":
-            response = {
-                "text": 'you pressed NO'
-            }
+            response = { "text": 'you pressed NO' }
+            break;
+        case "GET_STARTED":
+            response = { "text": 'hello welcome to webfactory' }
             break;
         default:
-            console.log("run default switch case")
+            response = { "text": ' ' }
 
     }
 
